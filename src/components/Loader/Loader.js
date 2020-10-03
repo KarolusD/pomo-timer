8d890cb7f3798c6aefa34f56e78cdd42bb2690b4
--local <karolus@JARVIS.local>
2020-10-03 18:36:11 +0200

    loader added

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,11 +3,15 @@ import styles from './Loader.module.css'
 
 const Loader = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.yellow}></div>
-      <div className={styles.red}></div>
-      <div className={styles.blue}></div>
-      <div className={styles.violet}></div>
+    <div className={styles.wrapper}>
+      <span className={styles.circle1}></span>
+      <span className={styles.circle2}></span>
+      <span className={styles.circle3}></span>
+      <span className={styles.circle4}></span>
+      <span className={styles.circle5}></span>
+      <span className={styles.circle6}></span>
+      <span className={styles.circle7}></span>
+      <span className={styles.circle8}></span>
     </div>
   )
 }
